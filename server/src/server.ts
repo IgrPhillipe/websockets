59fc47a16936577efd073f61c426a27e18ba5e68
IgrPhillipe <IgrPhillipe@gmail.com>
2023-03-07 13:30:18 -0300

    feat: finish api

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,20 +1,112 @@
-import express from "express";
+import express from 'express';
 import http from 'http';
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
+
+interface Room {
+  id: string;
+  name: string;
+}
+
+interface User {
+  id: string;
+  name: string;
+  lastName: string;
+  room: Room;
+}
+
+interface Message {
+  id: string;
+  message: string;
+  room: Room;
+  user: User;
+}
 
 const app = express();
 const httpServer = http.createServer(app);
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*"
+    origin: '*',
   },
 });
 
-httpServer.listen(3001, () => {
-  console.log(`🚀 Server is running on port 3001`);
+const rooms: Room[] = [];
 
-  io.on("connection", (socket) => {
-    socket.emit("connected", "You are connected to the server.");
+const users: User[] = [];
+
+const messages: Message[] = [];
+
+httpServer.listen(3001, () => {
+  console.log('🚀 Server is running on port 3001');
+
+  io.on('connection', (socket) => {
+    socket.emit('connected', 'You are connected to the server.');
+
+    socket.on('join-room', (data) => {
+      const { room, user } = data;
+
+      if (room) {
+        socket.join(room.id);
+
+        const selectedRoom = rooms.find((item: Room) => item.id === room.id);
+        const selectedUserInRoom = users.find(
+          (item: User) => item.id === user.id && item.room.id === room.id,
+        );
+
+        if (!selectedRoom) {
+          rooms.push(room);
+        }
+
+        if (!selectedUserInRoom) {
+          users.push({ ...user, ...room });
+        }
+
+        socket.broadcast.emit('all-rooms', rooms.map(
+          (item: Room) => ({
+            ...(item),
+            messages: messages.filter((aux: Message) => aux.room.id === item.id),
+          }),
+        ));
+
+        socket.emit('all-rooms', rooms.map(
+          (item: Room) => ({
+            ...(item),
+            messages: messages.filter((aux: Message) => aux.room.id === item.id),
+          }),
+        ));
+      }
+    });
+
+    socket.on('new-message', (data) => {
+      const {
+        id,
+        message,
+        room,
+        user,
+      } = data;
+
+      const messageData = {
+        id,
+        message,
+        room,
+        user,
+      };
+
+      messages.push(messageData);
+
+      socket.broadcast.emit('all-rooms', rooms.map(
+        (item: Room) => ({
+          ...(item),
+          messages: messages.filter((aux: Message) => aux.room.id === item.id),
+        }),
+      ));
+    });
+
+    socket.emit('all-rooms', rooms.map(
+      (item: Room) => ({
+        ...(item),
+        messages: messages.filter((aux: Message) => aux.room.id === item.id),
+      }),
+    ));
   });
 });
